586fb58816c3427f9e53df18b57595a54fce4696
mariamrashid <giannarashid@gmail.com>
2025-08-21 12:01:44 +0300

    Complete looping functions: happy_new_year, square_integers, fizzbuzz

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python3
-
+# 1. Countdown from 10 to 1, then "Happy New Year!"
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
+# 2. Return a new list with each integer squared
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
+# 3. Print numbers 1 to 100 with FizzBuzz logic
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
